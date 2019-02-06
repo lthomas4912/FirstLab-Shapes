ae1420aad4f836986bef1f47f55b838b52ea294d
Leah <leah29726@gmail.com>
2019-02-05 20:38:02 -0500

    House and tree

--- Picture/Picture.java
+++ Picture/Picture.java
@@ -14,8 +14,14 @@ public class Picture
 {
     private Square wall;
     private Square window;
+    private Square window2;
     private Triangle roof;
     private Circle sun;
+    private Circle top1;
+    private Circle top2;
+    private Circle top3;
+    private Square trunk;
+    private Square door;
 
     /**
      * Constructor for objects of class Picture
@@ -32,27 +38,75 @@ public class Picture
     {
         wall = new Square();
         wall.moveVertical(80);
-        wall.changeSize(100);
+        wall.changeSize(100,100);
+        wall.changeColor("green");
         wall.makeVisible();
 
         window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(20);
+        window.changeColor("white");
+        window.moveHorizontal(15);
         window.moveVertical(100);
+        window.changeSize(20,20);
         window.makeVisible();
+        
+        window2 = new Square();
+        window2.changeColor("white");
+        window2.changeSize(20,20);
+        window2.moveHorizontal(65);
+        window2.moveVertical(100);
+        window2.makeVisible();
 
         roof = new Triangle();
         roof.changeSize(50, 140);
         roof.moveHorizontal(60);
         roof.moveVertical(70);
+        roof.changeColor("black");
         roof.makeVisible();
 
         sun = new Circle();
         sun.changeColor("yellow");
         sun.moveHorizontal(180);
-        sun.moveVertical(-10);
+        sun.moveVertical(-15);
         sun.changeSize(60);
         sun.makeVisible();
+        
+        trunk = new Square();
+        trunk.changeColor("brown");
+        trunk.moveHorizontal(175);
+        trunk.moveVertical(100);
+        trunk.changeSize(15,70);
+        trunk.makeVisible();
+        
+        top1 = new Circle();
+        top1.changeColor("green");
+        top1.moveHorizontal(185);
+        top1.moveVertical(70);
+        top1.changeSize(40);
+        top1.makeVisible();
+        
+        top2 = new Circle();
+        top2.changeColor("green");
+        top2.moveHorizontal(195);
+        top2.moveVertical(65);
+        top2.changeSize(45);
+        top2.makeVisible();
+        
+        top3 = new Circle();
+        top3.changeColor("green");
+        top3.moveHorizontal(200);
+        top3.moveVertical(70);
+        top3.changeSize(40);
+        top3.makeVisible();
+        
+        door = new Square();
+        door.changeColor("red");
+        door.moveHorizontal(35);
+        door.moveVertical(140);
+        door.changeSize(30,40);
+        door.makeVisible();
+        
+      
+        
     }
 
     /**
--- Picture/Square.java
+++ Picture/Square.java
@@ -9,7 +9,8 @@ import java.awt.*;
 
 public class Square
 {
-    private int size;
+    private int width;
+    private int height;
     private int xPosition;
     private int yPosition;
     private String color;
@@ -20,7 +21,8 @@ public class Square
      */
     public Square()
     {
-        size = 30;
+        width = 10;
+        height = 60;
         xPosition = 60;
         yPosition = 50;
         color = "red";
@@ -148,10 +150,11 @@ public class Square
     /**
      * Change the size to the new size (in pixels). Size must be >= 0.
      */
-    public void changeSize(int newSize)
+    public void changeSize(int newWidth, int newHeight)
     {
         erase();
-        size = newSize;
+        width = newWidth;
+        height = newHeight;
         draw();
     }
 
@@ -173,7 +176,7 @@ public class Square
         if(isVisible) {
             Canvas canvas = Canvas.getCanvas();
             canvas.draw(this, color,
-                    new Rectangle(xPosition, yPosition, size, size));
+                    new Rectangle(xPosition, yPosition, width, height));
             canvas.wait(10);
         }
     }
